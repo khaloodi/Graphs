59793b717cc4e0f107fefc9d35afd808e089a276
Khaled Adad <khaledadad@lewisu.edu>
2020-01-22 21:47:59 -0600

    Added notes

---
+++ projects/graph/memorize.py
@@ -0,0 +1,14 @@
+# create an empty queue and enqueue A PATH TO the starting vertex ID
+# create a Set to store visited vertices
+# while the queue is not empty...
+    # dequeue the first PATH 
+    # grab the last vertex from the PATH 
+    # if that vertex has not been visited...
+        # check if it's the target 
+            # if so, return path 
+        # mark it as visited... 
+        # then add a path to its neighbors to the back of the queue 
+            # copy the path 
+            # append the neighbor to the back 
+
+
